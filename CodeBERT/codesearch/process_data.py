12450e39ceebedb4ebaf9960c58783cb1a67a72e
uttamkn <uttamkn15@gmail.com>
2025-05-26 21:57:57 +0530

    feat: add --use-hf and --languages CLI flags
    --use-hf enables loading HuggingFace models
    --languages allows specifying target languages for processing

--- CodeBERT/codesearch/process_data.py
+++ CodeBERT/codesearch/process_data.py
@@ -7,6 +7,13 @@ import os
 import json
 import numpy as np
 from more_itertools import chunked
+import argparse
+
+try:
+    from datasets import load_dataset
+except ImportError:
+    load_dataset = None
+
 
 DATA_DIR='../data/codesearch'
 
@@ -16,45 +23,58 @@ def format_str(string):
     return string
 
 
-def preprocess_test_data(language, test_batch_size=1000):
-    path = os.path.join(DATA_DIR, '{}_test_0.jsonl.gz'.format(language))
-    print(path)
-    with gzip.open(path, 'r') as pf:
-        data = pf.readlines()  
+def preprocess_test_data(language, test_batch_size=1000, use_hf=False):
+    if use_hf:
+        if load_dataset is None:
+            raise ImportError("Please install the 'datasets' library: pip install datasets")
+
+        print("Downloading dataset for language: {} from Hugging Face".format(language))
+        dataset = load_dataset("code_search_net", language, split="test")
+        data = dataset.to_pandas().to_dict(orient="records")
+        print("Downloaded {} examples".format(len(data)))
+    else:
+        path = os.path.join(DATA_DIR, '{}_test_0.jsonl.gz'.format(language))
+        print("Reading local dataset: {}".format(path))
+        with gzip.open(path, 'r') as pf:
+            lines = pf.readlines()
+        data = [json.loads(str(line, encoding='utf-8')) for line in lines]
 
     idxs = np.arange(len(data))
-    data = np.array(data, dtype=np.object)
-
-    np.random.seed(0)   # set random seed so that random things are reproducible
+    np.random.seed(0)
     np.random.shuffle(idxs)
-    data = data[idxs]
+    data = np.array(data, dtype=np.object)[idxs]
     batched_data = chunked(data, test_batch_size)
 
-    print("start processing")
+    print("Start processing")
     for batch_idx, batch_data in enumerate(batched_data):
         if len(batch_data) < test_batch_size:
-            break # the last batch is smaller than the others, exclude.
+            break  # the last batch is smaller than the others, exclude
+
         examples = []
-        for d_idx, d in enumerate(batch_data): 
-            line_a = json.loads(str(d, encoding='utf-8'))
+        for d_idx in range(len(batch_data)):
+            line_a = batch_data[d_idx]
             doc_token = ' '.join(line_a['docstring_tokens'])
-            for dd in batch_data:
-                line_b = json.loads(str(dd, encoding='utf-8'))
+            for line_b in batch_data:
                 code_token = ' '.join([format_str(token) for token in line_b['code_tokens']])
-
                 example = (str(1), line_a['url'], line_b['url'], doc_token, code_token)
-                example = '<CODESPLIT>'.join(example)
-                examples.append(example)
+                examples.append('<CODESPLIT>'.join(example))
 
-        data_path = os.path.join(DATA_DIR, 'test/{}'.format(language))
+        data_path = os.path.join(DATA_DIR, 'test', language)
         if not os.path.exists(data_path):
             os.makedirs(data_path)
+
         file_path = os.path.join(data_path, 'batch_{}.txt'.format(batch_idx))
-        print(file_path)
+        print("Writing to: {}".format(file_path))
         with open(file_path, 'w', encoding='utf-8') as f:
-            f.writelines('\n'.join(examples))
+            f.write('\n'.join(examples))
+
 
 if __name__ == '__main__':
-    languages = ['go', 'php', 'python', 'java', 'javascript', 'ruby']
-    for lang in languages:
-        preprocess_test_data(lang)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--use-hf', action='store_true', help="Use Hugging Face datasets instead of local gz files.")
+    parser.add_argument('--languages', nargs='+', default=['go', 'php', 'python', 'java', 'javascript', 'ruby'],
+                        help="List of programming languages to process (space-separated). Default: all.")
+    args = parser.parse_args()
+
+    for lang in args.languages:
+        preprocess_test_data(lang, use_hf=args.use_hf)
